82ae556369ccca0fc6b77d91167a5386baa29346
Leow14 <leonardoamorimdearaujo@hotmail.com>
2026-04-18 21:30:39 -0300

    Trying the props properties.

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,12 +1,15 @@
-import Button from './Button/button.jsx'
+import Student from "./Students.jsx"
 
 function App() {
-
-  return(
-    <>
-    <Button/>
-    </>
-  );
+    return(
+      <>
+          <Student name="Spongebob" age={40} isStudent={true}/>
+          <Student name="Patrick" age={40} isStudent={false}/>
+          <Student name="Squidwards" age={70} isStudent={false}/>
+          <Student name="Sandy" age={30} isStudent={true}/>
+          <Student/>
+      </>
+    );
 }
 
 export default App
