07ab14fe409b5464336d12b5c13354502dd9b90d
Leow14 <leonardoamorimdearaujo@hotmail.com>
2026-04-24 15:14:22 -0300

    Creating a color picker app with event handler.

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,11 +1,12 @@
 import MyComponent from "./MyComponent.jsx"
 import Counter from "./Counter.jsx"
+import ColorPicker from "./ColorPicker.jsx";
 
 function App() {
 
     return(
         <>
-        <MyComponent/>
+        <ColorPicker/>
         </>
     );
 }
