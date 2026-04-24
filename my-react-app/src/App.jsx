88e4b6e4c095b015997d19f13361a514deb4d1b7
Leow14 <leonardoamorimdearaujo@hotmail.com>
2026-04-23 22:17:39 -0300

    Introduction to click events on react. Click events are interactions when a user clicks on a specific element.

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,24 +1,10 @@
-import List from "./list.jsx";
-
+import Button from "./Button.jsx";
+import ProfilePicture from "./ProfilePicture.jsx";
 function App() {
-    
-    const fruits = [{id: 1, name:"lime",       calories: 95},
-                    {id: 2, name:"blueberry",  calories: 105},
-                    {id: 3, name:"peach",      calories: 36},
-                    {id: 4, name:"pineapple",  calories: 37}, 
-                    {id: 5, name:"kiwi",       calories: 50}];
-
-    const vegetables = [{id: 6, name:"potato",       calories: 125},
-                        {id: 7, name:"onion",  calories: 10},
-                        {id: 8, name:"garlic",      calories: 12},
-                        {id: 9, name:"cucumber",  calories: 56}, 
-                        {id: 10, name:"spinach",       calories: 5}];
-
 
     return(
         <>
-        {fruits.length     > 0 && <List items={fruits}    />}
-        {vegetables.length > 0 && <List items={vegetables} category="Vegetables"/>}
+        <ProfilePicture/>
         </>
     );
 }
---
+++ my-react-app/src/Button.jsx
@@ -0,0 +1,28 @@
+
+function Button(){
+
+    let count = 0;
+    
+    const handleClick = (e) => {
+        e.target.textContent = "OUCH! 😢";
+    }
+
+    const handleClick2 = (name) => {
+        if (count < 3 ){
+            count++,
+            console.log(`${name} you clicked me ${count} time/s`);
+        }
+        else{
+            console.log(`${name} stop clicking me!`);
+        }
+        };
+
+    const handleClick3 = (name) => console.log(`${name} stop clicking me`);
+
+    return(
+            <>
+            <button onDoubleClick={(e) => handleClick(e)}>Click me 👁️</button>
+            </>);
+}
+
+export default Button
