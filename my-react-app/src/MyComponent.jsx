c47b656cca4ea2be22dac3db4666e040c457d489
Leow14 <leonardoamorimdearaujo@hotmail.com>
2026-04-24 17:28:03 -0300

    Testing updater functions on react.

--- my-react-app/src/MyComponent.jsx
+++ my-react-app/src/MyComponent.jsx
@@ -3,67 +3,31 @@ import React, {useState} from "react";
 
 function MyComponent(){
 
-    const [name, setName] = useState("Guest");
-    const [quantity, setQuantity] = useState(1);
-    const [comment, setComment] = useState("");
-    const [payment, setPayment] = useState("");
-    const [shipping, setShipping] = useState("Delivery");
+    const [car, setCar] = useState({year:2024, 
+                                    make: "Ford", 
+                                    model: "Mustang"});
 
-    function handleNameChange(event){
-        setName(event.target.value);
+    
+    function handleYearChange(event){
+        setCar(previousCar => ({...previousCar, year: event.target.value}));
     }
 
-    function handleQuantityChange(event){
-        setQuantity(event.target.value);
+    function handleMakeChange(event){
+        setCar(previousCar => ({...previousCar, make: event.target.value}));
     }
-
-    function handleCommentChange(event){
-        setComment(event.target.value);
-    }
-
-    function handlePaymentChange(event){
-        setPayment(event.target.value);
-    }
-
-    function handleShippingChange(event){
-        setShipping(event.target.value);
+    
+    function handleModelChange(event){
+        setCar(previousCar => ({...previousCar, model: event.target.value}));
     }
 
     return(
-        <>
-        <input value={name} onChange={handleNameChange}/>
-        <p>Name: {name}</p>
+        <div>
+            <p>Your favorite car is: {car.year} {car.make} {car.model}</p>
 
-        <input value={quantity} onChange={handleQuantityChange} type="number"/>
-        <p>Quantity: {quantity}</p>
-
-        <textarea value={comment} onChange={handleCommentChange} placeholder="Enter delivery instructions"></textarea>
-        <p>Comment: {comment}</p>
-
-        <select value={payment} onChange={handlePaymentChange}>
-            <option value= "">Select an option</option>
-            <option value= "Visa">Visa</option>
-            <option value= "Mastercard">Mastercard</option>
-            <option value= "Giftcard">Giftcard</option>
-        </select>
-        <p>Payment: {payment}</p>
-
-        <label>
-            <input type="radio" value="Pick up"
-            checked={shipping === "Pick up"}
-            onChange={handleShippingChange}/>
-            Pick up
-        </label><br/>
-
-        <label>
-            <input type="radio" value="Delivery"
-            checked={shipping === "Delivery"}
-            onChange={handleShippingChange}/>
-            Delivery
-        </label>
-
-        <p>Shipping: {shipping}</p>
-        </>
+            <input type="number" value={car.year} onChange={handleYearChange}/> <br/>
+            <input type="text" value={car.make} onChange={handleMakeChange}/> <br/>
+            <input type="text" value={car.model} onChange={handleModelChange}/> <br/>
+        </div>
     )
 }
 
