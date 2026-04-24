5818a0cf9df36978d0ec885f996501c44ca2d88e
Leow14 <leonardoamorimdearaujo@hotmail.com>
2026-04-23 21:53:35 -0300

    Trying arrow function and lists on react. Part 2.

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,9 +1,24 @@
 import List from "./list.jsx";
 
 function App() {
+    
+    const fruits = [{id: 1, name:"lime",       calories: 95},
+                    {id: 2, name:"blueberry",  calories: 105},
+                    {id: 3, name:"peach",      calories: 36},
+                    {id: 4, name:"pineapple",  calories: 37}, 
+                    {id: 5, name:"kiwi",       calories: 50}];
+
+    const vegetables = [{id: 6, name:"potato",       calories: 125},
+                        {id: 7, name:"onion",  calories: 10},
+                        {id: 8, name:"garlic",      calories: 12},
+                        {id: 9, name:"cucumber",  calories: 56}, 
+                        {id: 10, name:"spinach",       calories: 5}];
+
+
     return(
         <>
-        <List></List>
+        {fruits.length     > 0 && <List items={fruits}    />}
+        {vegetables.length > 0 && <List items={vegetables} category="Vegetables"/>}
         </>
     );
 }
--- my-react-app/src/list.jsx
+++ my-react-app/src/list.jsx
@@ -1,26 +1,16 @@
+import PropTypes from "prop-types";
 
+function List({category = "Category", items = []}){
 
-function List(){
+    const listItems = items.map     (item => (
+    <li key={item.id}>
+        {item.name}: &nbsp; <b>{item.calories}</b>
+    </li>));
 
-    const fruits = [{id: 1, name:"lime",       calories: 95},
-                    {id: 2, name:"blueberry",  calories: 105},
-                    {id: 3, name:"peach",      calories: 36},
-                    {id: 4, name:"pineapple",  calories: 37}, 
-                    {id: 5, name:"kiwi",       calories: 50}];
-
-    fruits.sort((a, b) => a.name.localeCompare(b.name));    // Alphabetical
-    fruits.sort((a, b) => b.name.localeCompare(a.name));    // Reverse
-    fruits.sort((a, b) => a.calories - b.calories);         // Numeric
-    fruits.sort((a, b) => b.calories - a.calories);         // Reverse Numeric
-    
-    const lowCalFruits = fruits.filter(fruit => fruit.calories < 100)
-    const highCalFruits = fruits.filter(fruit => fruit.calories > 100)
-
-
-    const ItemsList = highCalFruits.map     (highCalFruit => <li key={highCalFruit.id}>
-                                            {highCalFruit.name}: &nbsp;
-                                            <b>{highCalFruit.calories}</b></li>);
-
-    return (<ol>{ItemsList}</ol>);
+    return (<>
+                <h3 className="list-category">{category}</h3>
+                <ol className="list-items">{listItems}</ol>
+            </>);
 }
+
 export default List
